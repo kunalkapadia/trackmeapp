c7b0ca528a471f59e78b96d3d697fe87008f9fa1
Kunal Kapadia <kunalkapadia12@gmail.com>
2016-08-11 01:25:12 +0530

    new feature

--- Components/User.js
+++ Components/User.js
@@ -12,6 +12,7 @@ class User extends Component {
     super(props)
     this.state = {
       username: '',
+      password: '',
       error: ''
     }
 
@@ -19,12 +20,42 @@ class User extends Component {
   }
 
   _onSubmit() {
-    if (this.state.username.length === 0) {
+    const that = this
+    this.hostUrl = 'http://ec2-54-191-149-78.us-west-2.compute.amazonaws.com:3000/api/users/signin'
+
+    if (this.state.username.length === 0 || this.state.password.length === 0) {
       this.setState({
-        error: 'Please enter your name'
+        error: 'Your username or password is empty'
       })
     } else {
-      Actions.addGeoLocation({username: this.state.username})
+      fetch(this.hostUrl, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username: this.state.username,
+          password: this.state.password
+        })
+      })
+      .then(function (response) {
+        return response.json()
+      })
+      .then(function (data) {
+        if (data.message === "no such user exists") {
+          that.setState({
+            error: "Error in authentication"
+          })
+        } else {
+          Actions.addGeoLocation({username: that.state.username})
+        }
+      }).catch(function (e) {
+        console.log(e)
+        that.setState({
+          error: "Error in authentication"
+        })
+      })
     }
   }
 
@@ -36,21 +67,36 @@ class User extends Component {
         </Text>
 
         <TextInput
-          ref="yourName"
+          ref="username"
           autoCapitalize={"words"}
           maxLength={30}
           style={{height: 45, borderColor: 'gray', borderWidth: 1}}
-          onChangeText={(name) => this.setState({username: name})}
-          value={this.state.yourName}
+          onChangeText={(username) => this.setState({username: username})}
+          value={this.state.username}
           autoFocus={true}
           returnKeyType="next"
-          placeholder="Your Name" />
+          placeholder="Username" />
 
-          <TouchableHighlight style={{marginTop: 10}} onPress={this._onSubmit}>
-            <Text style={styles.button}>
-              Next
-            </Text>
-          </TouchableHighlight>
+        <TextInput
+          ref="password"
+          autoCapitalize={"words"}
+          maxLength={30}
+          style={{height: 45, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={(password) => this.setState({password: password})}
+          value={this.state.password}
+          returnKeyType="next"
+          placeholder="Password" />
+
+        <TouchableHighlight style={{marginTop: 10}} onPress={this._onSubmit}>
+          <Text style={styles.button}>
+            Signup
+          </Text>
+        </TouchableHighlight>
+        <TouchableHighlight style={{marginTop: 10}} onPress={this._onSubmit}>
+          <Text style={styles.button}>
+            Login
+          </Text>
+        </TouchableHighlight>
       </View>
 
     );
